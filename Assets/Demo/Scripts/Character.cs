0bafe0e96d84d9ce2094c6e5447114fe6af75910
DerekNien <aegis60918@gmail.com>
2017-07-12 19:12:21 +0800

    - Add: skeleton with ai

---
+++ Assets/Demo/Scripts/Character.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+public class Character : MonoBehaviour
+{
+	//=================
+	// Constants
+	//=================
+	// animation bools
+	const string IDLE   = "isIdle";
+	const string WALK   = "isWalking";
+	const string RUN    = "isRunning";
+	const string ATTACK = "isAttacking";
+
+	//=================
+	// Data
+	//=================
+	Animator animator;
+	string currentBool = IDLE;
+
+
+	//=================
+	// Life Cycle
+	//=================
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	//=================
+	// Public Methods
+	//=================
+	public void Idle()
+	{
+		ResetBool(IDLE);
+		animator.SetBool(IDLE, true);
+	}
+
+	public void Walk()
+	{
+		ResetBool(WALK);
+		animator.SetBool(WALK, true);
+	}
+
+	public void Run()
+	{
+		ResetBool(RUN);
+		animator.SetBool(RUN, true);
+	}
+
+	public void Attack()
+	{
+		ResetBool(ATTACK);
+		animator.SetBool(ATTACK, true);
+	}
+
+	//=================
+	// Private Methods
+	//=================
+	void ResetBool(string boolTrigger)
+	{
+		if(currentBool != boolTrigger) {
+			animator.SetBool(currentBool, false);
+			currentBool = boolTrigger;
+		}
+	}
+}
---
+++ Assets/Demo/Scripts/Monster.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Monster : MonoBehaviour
+{
+	//=================
+	// Constants
+	//=================
+	enum State
+	{
+		PATROL,
+		CHASE,
+		ATTACK
+	}
+
+	//=================
+	// Editor Fields
+	//=================
+	[SerializeField] GameObject player;
+	[SerializeField] Character character;
+	[SerializeField] State state;
+	// Patrolling
+	[SerializeField] float patrolSpeed = 1f;
+	// Chasing
+	[SerializeField] float chaseDistance = 10f;
+	[SerializeField] float chaseSpeed = 2f;
+	// Attacking
+	[SerializeField] float attackDistance = 2f;
+
+	//=================
+	// Data
+	//=================
+	NavMeshAgent agent;
+	// Patrolling
+	GameObject[] waypoints;
+	int waypointIdx;
+	// Chasing
+	GameObject target;
+
+
+	//=================
+	// Life Cycle
+	//=================
+	void Awake()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+		waypointIdx = Random.Range(0, waypoints.Length);
+	}
+
+	void Start()
+	{
+		agent.updatePosition = true;
+		agent.updateRotation = true;
+
+		state = State.PATROL;
+	}
+
+	void Update()
+	{
+		UpdateState();
+	}
+
+	//=================
+	// Private Methods
+	//=================
+	void UpdateState()
+	{
+		switch(state)
+		{
+		case State.PATROL:
+			Patrol();
+			break;
+		case State.CHASE:
+			Chase();
+			break;
+		case State.ATTACK:
+			Attack();
+			break;
+		}
+	}
+
+	void Patrol()
+	{
+		if(Vector3.Distance(transform.position, player.transform.position) <= chaseDistance)
+		{
+			target = player;
+			state = State.CHASE;
+		}
+		else
+		{
+			agent.speed = patrolSpeed;
+			character.Walk();
+			if(Vector3.Distance(transform.position, waypoints[waypointIdx].transform.position) >= 5)
+			{
+				agent.SetDestination(waypoints[waypointIdx].transform.position);
+			}
+			else
+			{
+				waypointIdx = Random.Range(0, waypoints.Length);
+			}
+		}
+	}
+
+	void Chase()
+	{
+		if(Vector3.Distance(transform.position, player.transform.position) <= attackDistance)
+		{
+			state = State.ATTACK;
+		}
+		else if(Vector3.Distance(transform.position, player.transform.position) >= chaseDistance)
+		{
+			state = State.PATROL;
+		}
+		else
+		{
+			agent.speed = chaseSpeed;
+			character.Run();
+			agent.SetDestination(target.transform.position);
+		}
+	}
+
+	void Attack()
+	{
+		if(Vector3.Distance(transform.position, player.transform.position) <= attackDistance)
+		{
+			agent.speed = 0;
+			character.Attack();
+		}
+		else if(Vector3.Distance(transform.position, player.transform.position) <= chaseDistance)
+		{
+			target = player;
+			state = State.CHASE;
+		}
+		else
+		{
+			state = State.PATROL;
+		}
+	}
+}
